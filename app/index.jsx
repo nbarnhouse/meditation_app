d6acb8621b2663347422c052c4cfa879f4019f96
Nicole Barnhouse <nicolebarnhouse@gmail.com>
2025-05-07 21:04:39 -0500

    added gradient to home screen

--- app/index.jsx
+++ app/index.jsx
@@ -14,34 +14,53 @@ import { StatusBar } from "expo-status-bar";
 
 //ES Module Import
 import beachImage from "@/assets/meditation-images/beach.webp";
+import { LinearGradient } from "expo-linear-gradient";
 import CustomButton from "./components/CustomButton";
 
 export default function App() {
   const router = useRouter();
   return (
-    <ImageBackground source={beachImage} resizeMode="cover" style={{ flex: 1 }}>
-      <SafeAreaView style={styles.container}>
-        <View style={{ alignItems: "center" }}>
-          <Text style={styles.title}>Simple Meditation App</Text>
-          <Text style={styles.subtitle}>
-            Simplifying Meditation for Everyone
-          </Text>
-        </View>
-        <CustomButton
-          title="Get Started"
-          onPress={() => router.push("/meditationOptionScreen")}
-        />
-        <StatusBar style="light" />
-      </SafeAreaView>
+    <ImageBackground source={beachImage} style={styles.background}>
+      <LinearGradient
+        colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)", "transparent"]}
+        style={styles.gradient}
+      >
+        <SafeAreaView style={{ flex: 1 }}>
+          <View style={styles.container}>
+            <View style={styles.header}>
+              <Text style={styles.title}>Simple Meditation App</Text>
+              <Text style={styles.subtitle}>
+                Simplifying Meditation for Everyone
+              </Text>
+            </View>
+            <CustomButton
+              title="Get Started"
+              onPress={() => router.push("/meditationOptionScreen")}
+            />
+          </View>
+          <StatusBar style="light" />
+        </SafeAreaView>
+      </LinearGradient>
     </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+  gradient: {
+    flex: 1,
+    padding: 20,
+  },
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "space-around",
+  },
+  header: {
+    alignItems: "center",
   },
   title: {
     color: "white",
